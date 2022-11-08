1d0d177db23c3ccb26b7fd82d9fc4f4cc07b4908
it180184 <f.ettinger@students.htl-leonding.ac.at>
2022-11-08 09:18:33 +0100

    #5 click dummy first version

---
+++ project/client/leoGreen/src/app/quiz/quiz-host/quiz-host.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-host',
+  templateUrl: './quiz-host.component.html',
+  styleUrls: ['./quiz-host.component.scss']
+})
+export class QuizHostComponent implements OnInit {
+
+  game = {
+    code: '123456',
+    players: [
+      {
+        name: 'Fabian'
+      },
+      {
+        name: 'Lorenz'
+      },
+      {
+        name: 'Dominik'
+      },
+      {
+        name: 'Marcel'
+      }
+    ]
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
