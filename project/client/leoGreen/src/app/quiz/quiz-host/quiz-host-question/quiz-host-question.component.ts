3c1df4d5e818f96e3b16fa31d48f4a404d94c52f
it180184 <f.ettinger@students.htl-leonding.ac.at>
2022-11-21 12:39:51 +0100

    #5 click dummy questions first version

---
+++ project/client/leoGreen/src/app/quiz/quiz-host/quiz-host-question/quiz-host-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-host-question',
+  templateUrl: './quiz-host-question.component.html',
+  styleUrls: ['./quiz-host-question.component.scss']
+})
+export class QuizHostQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
