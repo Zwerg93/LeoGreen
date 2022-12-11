17182f145c4b52bbef70737a5b54ae69bb834a8a
it180184 <f.ettinger@student.htl-leonding.ac.at>
2022-12-11 22:13:40 +0100

    #5 quiz question styling

--- project/client/leoGreen/src/app/services/game.service.ts
+++ project/client/leoGreen/src/app/services/game.service.ts
@@ -24,7 +24,7 @@ export class GameService {
         name: 'Marcel'
       },
       {
-        name: 'Thisisaverylongusername'
+        name: 'Thisisaverylongname'
       },
       {
         name: 'Marcel'
