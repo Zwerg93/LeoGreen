52e8be9b2e43416e027bb0bcc08894b2e6d29af1
it180184 <f.ettinger@student.htl-leonding.ac.at>
2023-01-06 23:33:10 +0100

    #15 create Entities and slight changes to POJOS

--- project/leogreen_server/src/main/java/at/htl/model/pojo/Game.java
+++ project/leogreen_server/src/main/java/at/htl/model/pojo/Game.java
@@ -1,21 +1,20 @@
 package at.htl.model.pojo;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class Game implements Serializable {
     Integer code;
     Boolean started;
-    public ArrayList<Player> players;
-    Question[] questions;
+    List<Player> players;
+    List<Question> questions;
     Integer currentQuestion;
 
-    public Game(Integer code, Boolean started, Player[] players, Question[] questions, Integer currentQuestion) {
+    public Game(Integer code, Boolean started, List<Player> players, List<Question> questions, Integer currentQuestion) {
         this.code = code;
         this.started = started;
-        this.players.addAll(Arrays.stream(players).toList());
+        this.players = players;
         this.questions = questions;
         this.currentQuestion = currentQuestion;
     }
--- project/leogreen_server/src/main/java/at/htl/model/pojo/Question.java
+++ project/leogreen_server/src/main/java/at/htl/model/pojo/Question.java
@@ -1,12 +1,13 @@
 package at.htl.model.pojo;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Question implements Serializable {
     String header;
-    Answer[] answers;
+    List<Answer> answers;
 
-    public Question(String header, Answer[] answers) {
+    public Question(String header, List<Answer> answers) {
         this.header = header;
         this.answers = answers;
     }
