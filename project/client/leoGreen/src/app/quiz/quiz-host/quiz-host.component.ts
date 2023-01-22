83f7cd5c6b22e94011b0bff6640743fd82113402
it180184 <f.ettinger@student.htl-leonding.ac.at>
2023-01-22 15:40:19 +0100

    #22 general styling and qr code stuff

--- project/client/leoGreen/src/app/quiz/quiz-host/quiz-host.component.ts
+++ project/client/leoGreen/src/app/quiz/quiz-host/quiz-host.component.ts
@@ -3,8 +3,9 @@ import {GameService} from "../../services/game.service";
 import {HttpService} from "../../services/http.service";
 import {Quiz} from "../../model/quiz";
 import {Game} from "../../model/game";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Subscription} from "rxjs";
+import {PlatformLocation} from "@angular/common";
 
 @Component({
   selector: 'app-quiz-host',
@@ -17,14 +18,19 @@ export class QuizHostComponent implements OnInit {
   subscription ?: Subscription
   game?: Game;
   qrCodeSize: number = window.innerWidth / 7;
+  qrCodeLink?: string;
 
-  constructor(private http: HttpService, private quizService: GameService, private aRoute: ActivatedRoute) {
+  constructor(private http: HttpService, private quizService: GameService,
+              private aRoute: ActivatedRoute, private platformLocation: PlatformLocation) {
     if (aRoute.snapshot.paramMap.get("id")) {
       let quizId = Number(aRoute.snapshot.paramMap.get("id"))
-        this.http.postStartGame(quizId).subscribe( gameId => {this.quizService.startWebsocket(gameId).subscribe(value => {
+      this.http.postStartGame(quizId).subscribe(gameId => {
+        this.quizService.startWebsocket(gameId).subscribe(value => {
           this.game = value
-          console.log(value)
-        })})
+          if (this.game)
+            this.qrCodeLink = `${(platformLocation as any).location.origin}/quiz/client?code=${this.game.id}`;
+        })
+      })
     }
   }
 
