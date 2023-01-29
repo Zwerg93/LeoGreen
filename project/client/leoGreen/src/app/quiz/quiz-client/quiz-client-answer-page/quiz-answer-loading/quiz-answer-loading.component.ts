c68ffd421f567216dc269ae06152e50235f20563
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-01-29 18:15:54 +0100

    added QuizAnswerLoadingComponent

---
+++ project/client/leoGreen/src/app/quiz/quiz-client/quiz-client-answer-page/quiz-answer-loading/quiz-answer-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-answer-loading',
+  templateUrl: './quiz-answer-loading.component.html',
+  styleUrls: ['./quiz-answer-loading.component.scss']
+})
+export class QuizAnswerLoadingComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
